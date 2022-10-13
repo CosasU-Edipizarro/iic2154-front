88f05a77051bb70d00223b2bc571a80701bcadfe
Edison Pizarro Godoy <edipizarro@uc.cl>
2022-10-13 13:28:22 -0300

    first commit

---
+++ src/__tests__/init.test.js
@@ -0,0 +1,13 @@
+// import React from 'react';
+// import { render, screen } from '@testing-library/react';
+// import App from '../App';
+
+it('Test true', () => {
+  expect(true).toEqual(true);
+});
+
+
+// it('renders init site', () => {
+//   render(<App />);
+//   expect(screen.getByText('Dashboard')).toBeInTheDocument();
+// });
